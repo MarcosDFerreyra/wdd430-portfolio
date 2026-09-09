7dd9be0aab90bc91e85b044a6405b3510a134fb5
MarcosDFerreyra <marcosferrey30>
2026-09-09 18:33:58 -0300

    color changes and link to site

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,12 @@ import ContactInfo from "@/components/contact";
 
 export default function About() {
     return (
-        <main className="max-w-4xl mx-auto px-4 py-12">
+        <main className="text-white max-w-4xl mx-auto px-4 py-12">
             <h2 className="text-3xl font-bold mb-4">About Me</h2>
-            <p className="text-lg text-gray-700">
+            <p className="text-white text-lg text-gray-700">
                 This about page shares more information about my background and work.
             </p>
-            <ContactInfo name="Marcos Ferreyra" email="marcosferrey30@gmail.com" phone="+54 11 6248 0976" address="Florencion Varela, Buenos Aires, Argentina" />
+            <ContactInfo name="Marcos Ferreyra" email="marcos30@gmail.com" phone="+54 1234 5678" address="Florencion Varela, Buenos Aires, Argentina" />
         </main>
     );
 }
--- components/contact.tsx
+++ components/contact.tsx
@@ -8,12 +8,12 @@ interface ContactInfoProps {
 export default function ContactInfo({ name, email, phone, address }: ContactInfoProps) {
     return (
         <section className="max-w-4xl mx-auto px-4 py-12">
-            <h2 className="text-3xl font-bold mb-4">Contact Information</h2>
-            <p className="text-lg text-gray-700 mb-2"><strong>Name:</strong> {name}</p>
-            <p className="text-lg text-gray-700 mb-2"><strong>Email:</strong> {email}</p>
-            <p className="text-lg text-gray-700 mb-2"><strong>Phone:</strong> {phone}</p>
+            <h2 className="text-white text-3xl font-bold mb-4">Contact Information</h2>
+            <p className="text-white text-lg text-gray-700 mb-2"><strong>Name:</strong> {name}</p>
+            <p className="text-white text-lg text-gray-700 mb-2"><strong>Email:</strong> {email}</p>
+            <p className="text-white text-lg text-gray-700 mb-2"><strong>Phone:</strong> {phone}</p>
             {address && (
-                <p className="text-lg text-gray-700 mb-2"><strong>Address:</strong> {address}</p>
+                <p className="text-white text-lg text-gray-700 mb-2"><strong>Address:</strong> {address}</p>
             )}
         </section>
     );
